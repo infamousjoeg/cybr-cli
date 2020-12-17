4783613034e865394219207f185a5514e040763f
Andrew Copeland <50109276+AndrewCopeland@users.noreply.github.com>
2020-12-16 20:18:35 -0500

    Added tests for creating, retrieving and removing the pasapi config (#13)

--- pkg/cybr/api/client_test.go
+++ pkg/cybr/api/client_test.go
@@ -2,6 +2,9 @@ package api_test
 
 import (
 	"os"
+	"testing"
+
+	pasapi "github.com/infamousjoeg/cybr-cli/pkg/cybr/api"
 )
 
 var (
@@ -9,3 +12,66 @@ var (
 	username = os.Getenv("PAS_USERNAME")
 	password = os.Getenv("PAS_PASSWORD")
 )
+
+func TestIsValidSuccess(t *testing.T) {
+	validAuthTypes := []string{"cyberark", "ldap"}
+
+	for _, validAuthType := range validAuthTypes {
+		client := pasapi.Client{
+			AuthType: validAuthType,
+		}
+
+		err := client.IsValid()
+		if err != nil {
+			t.Errorf("Auth type '%s' is valid however client is returning not valid. %s", validAuthType, err)
+		}
+	}
+}
+
+func TestIsValidFail(t *testing.T) {
+	validAuthTypes := []string{"invalidAuthType", "", "123456"}
+
+	for _, validAuthType := range validAuthTypes {
+		client := pasapi.Client{
+			AuthType: validAuthType,
+		}
+
+		err := client.IsValid()
+		if err == nil {
+			t.Errorf("Auth type '%s' is not valid however client is returning valid. %s", validAuthType, err)
+		}
+	}
+}
+
+func TestSetGetRemoveConfigSuccess(t *testing.T) {
+	// I tested these three functions together so we can create, read and delete
+	// without worrying about the order of test execution
+	client := pasapi.Client{
+		BaseURL:      hostname,
+		AuthType:     "cyberark",
+		InsecureTLS:  false,
+		SessionToken: "",
+	}
+
+	err := client.SetConfig()
+	if err != nil {
+		t.Errorf("Failed to set the pasapi configuration. %s", err)
+	}
+
+	client, err = pasapi.GetConfig()
+	if err != nil {
+		t.Errorf("Failed to retrieve pasapi config. %s", err)
+	}
+
+	err = client.RemoveConfig()
+	if err != nil {
+		t.Errorf("Failed to remove pasapi config. %s", err)
+	}
+}
+
+func TestGetConfigFail(t *testing.T) {
+	_, err := pasapi.GetConfig()
+	if err == nil {
+		t.Errorf("Successfully retrieved pasapi config but pasapi config should not exist")
+	}
+}
