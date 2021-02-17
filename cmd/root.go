b84ea8acd3b9bfaac6337d86157a86e65d4f01c3
AndrewCopeland <andcope1995@gmail.com>
2021-02-17 14:57:41 -0500

    Remove commented lines of code

--- cmd/root.go
+++ cmd/root.go
@@ -44,47 +44,9 @@ func getLogger() logger.CMD {
 
 func init() {
 	rootCmd.PersistentFlags().BoolVar(&Verbose, "verbose", false, "To enable verbose logging")
-	// cobra.OnInitialize(initConfig)
-
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.conceal.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-	// Joe note: This would be good to allow multiple secret providers
 }
 
 // GetCMD returns the root cmd
 func GetCMD() *cobra.Command {
 	return rootCmd
 }
-
-// initConfig reads in config file and ENV variables if set.
-// func initConfig() {
-// 	if cfgFile != "" {
-// 		// Use config file from the flag.
-// 		viper.SetConfigFile(cfgFile)
-// 	} else {
-// 		// Find home directory.
-// 		home, err := homedir.Dir()
-// 		if err != nil {
-// 			fmt.Println(err)
-// 			os.Exit(1)
-// 		}
-
-// 		// Search config in home directory with name ".conceal" (without extension).
-// 		viper.AddConfigPath(home)
-// 		viper.SetConfigName(".conceal")
-// 	}
-
-// 	viper.AutomaticEnv() // read in environment variables that match
-
-// 	// If a config file is found, read it in.
-// 	if err := viper.ReadInConfig(); err == nil {
-// 		fmt.Println("Using config file:", viper.ConfigFileUsed())
-// 	}
-// }
