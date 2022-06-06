401fd03cf32fabe48e3e22fd1578366cbc273133
Joe Garcia <infamousjoeg@users.noreply.github.com>
2022-06-06 14:56:56 -0400

    Fix #91 add support to query platforms in pas (#153)
    * Add platforms cmd
    
    * Fix accounts var name
    
    * Update docs
    
    * Fix linting errors

---
+++ pkg/cybr/api/queries/listplatforms.go
@@ -0,0 +1,7 @@
+package queries
+
+// ListPlatforms represents valid query parameters when listing platforms
+type ListPlatforms struct {
+	Active       bool   `query_key:"active"`
+	PlatformType string `query_key:"platformType"`
+}
