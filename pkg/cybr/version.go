ca36d43701894b957162fe9eac07ee069cc18200
Joe Garcia <joe@joe-garcia.com>
2023-08-14 13:56:46 -0400

    Bump version to 1.0.0-release

--- pkg/cybr/version.go
+++ pkg/cybr/version.go
@@ -3,11 +3,11 @@ package cybr
 import "fmt"
 
 // Version field is a SemVer that should indicate the baked-in version of conceal
-var Version = "0.1.14"
+var Version = "1.0.0"
 
 // Tag field denotes the specific build type for the broker. It may be replaced by compile-time variables if needed to
 // provide the git commit information in the final binary.
-var Tag = "beta"
+var Tag = "release"
 
 // FullVersionName is the user-visible aggregation of version and tag of this codebase
 var FullVersionName = fmt.Sprintf("%s-%s", Version, Tag)
